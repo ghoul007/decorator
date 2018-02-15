c5323ffd91150a994164ae7d745cf8586ddee729
Ahmed <ghoul.ahmed5@gmail.com>
2018-02-15 22:02:13 +0100

    custom decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { testDecorator } from "./testDecorator";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
+@testDecorator({name:"ghoul"})
 export class AppComponent {
-  title = 'app';
+  title = "app";
+
+  constructor() {
+    console.log("test", this['name']());
+  }
 }
---
+++ src/app/testDecorator.ts
@@ -0,0 +1,5 @@
+export function testDecorator(val) {
+  return (target)=>{
+    Object.defineProperty(target.prototype, "name", { value: () => val.name });
+  }
+}
